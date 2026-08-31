50a220e4a43ee7c9ac52c953e0db0752141dcec4
Anthraxxxxxxx <prasaibibekk@gmail.com>
2026-08-31 18:50:47 +0930

    week_5_excel

--- developer/ThuyChuongDuong_S385201/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ developer/ThuyChuongDuong_S385201/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6a88df28cbc72eeb0ccbf6da9310bab87b6143")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- developer/ThuyChuongDuong_S385201/Week 2/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ developer/ThuyChuongDuong_S385201/Week 2/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b71ca2b877e7f256b7596dc8c3d4730e714e5b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6a88df28cbc72eeb0ccbf6da9310bab87b6143")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
