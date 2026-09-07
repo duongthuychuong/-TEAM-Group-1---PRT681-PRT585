d908dbd0e8824de65268d7f7f16f3686e95942d0
Anthraxxxxxxx <prasaibibekk@gmail.com>
2026-09-07 22:06:06 +0930

    PowerBI

--- developer/ThuyChuongDuong_S385201/Week 2/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ developer/ThuyChuongDuong_S385201/Week 2/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6a88df28cbc72eeb0ccbf6da9310bab87b6143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a220e4a43ee7c9ac52c953e0db0752141dcec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
