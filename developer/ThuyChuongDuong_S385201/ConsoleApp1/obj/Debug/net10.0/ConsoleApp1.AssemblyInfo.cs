a9003015f4a797a68d024d3b03f4e942d6708895
Anthraxxxxxxx <prasaibibekk@gmail.com>
2026-09-08 12:49:22 +0930

    Tableau

--- developer/ThuyChuongDuong_S385201/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ developer/ThuyChuongDuong_S385201/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a220e4a43ee7c9ac52c953e0db0752141dcec4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f27d55992d77eac1a32d350316cd54a1f61c3575")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
